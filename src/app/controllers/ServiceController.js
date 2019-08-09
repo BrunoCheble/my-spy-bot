427b28ba30b3681bd708fb86d0bd5a93d6c7e933
brunocheble <contato.cheble@gmail.com>
2019-08-09 18:33:17 +0100

    Foi adicionado a busca dos filters respectivos ao service, na requisição services/find

--- src/app/controllers/ServiceController.js
+++ src/app/controllers/ServiceController.js
@@ -1,11 +1,12 @@
 const Service = require('../models/Service');
+const Filter = require('../models/Filter');
 const Bot = require('../models/Bot');
 
 class ServiceController {
 
     async find(req, res) {
         const service = await Service.findOne(req.query.id);
-        const filters = await filters.find({ _serviceId: service.id });
+        const filters = await Filter.find({ _serviceId: service.id });
 
         return res.json({...service, filters });
     }
