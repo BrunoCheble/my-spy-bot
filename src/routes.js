a3c1696553549f4cf1da6246e963edc3426cae36
brunocheble <contato.cheble@gmail.com>
2019-08-25 22:39:58 +0100

    Teste

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ const AdvertController = require('./app/controllers/AdvertController');
 
 const routes = new Router();
 
+routes.get('', ServiceController.start);
+
 routes.post('/start', ServiceController.start);
 
 routes.post('/services', ServiceController.store);
